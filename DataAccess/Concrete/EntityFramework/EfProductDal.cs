8ee1cf4b9f4106626fad1651909fd24a373aee97
Yaşar Mehmet Akat <akat434@gmail.com>
2023-07-27 18:53:17 +0300

    Core Katmanı oluşturuldu.

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -10,55 +11,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NortwindContext>,IProductDal
     {
-        public void Add(Product entity)
-        {
-            //IDisposable pattern implementation of c#
-            using (NortwindContext context = new NortwindContext())
-            {
-                var addedEntity = context.Entry(entity); //context teki veriye ulaş
-                addedEntity.State = EntityState.Added; // entity ekle
-                context.SaveChanges(); // işlemlerin hepsini yap.
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-            using (NortwindContext context = new NortwindContext())
-            {
-                var deletedEntity = context.Entry(entity); //context teki veriye ulaş
-                deletedEntity.State = EntityState.Deleted; // entity ekle
-                context.SaveChanges(); // işlemlerin hepsini yap.
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NortwindContext context = new NortwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NortwindContext context = new NortwindContext())
-            {
-                return filter == null
-                    ? context.Set<Product>().ToList()
-                    : context.Set<Product>().Where(filter).ToList();
-            } 
-        }
-
-        public void Update(Product entity)
-        {
-            using (NortwindContext context = new NortwindContext())
-            {
-                var updatedEntity = context.Entry(entity); //context teki veriye ulaş
-                updatedEntity.State = EntityState.Modified; // entity ekle
-                context.SaveChanges(); // işlemlerin hepsini yap.
-            }
-        }
+        
     }
 }
--- DataAccess/Concrete/EntityFramework/NortwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NortwindContext.cs
@@ -13,11 +13,12 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\\mssqllocaldb;Database=Nortwind;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"server=(localdb)\mssqllocaldb;database=northwind;trusted_connection=true;");
         }
 
         public DbSet<Product> Products { get; set;}
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
