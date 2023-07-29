68f0ff5dc61e77714d8ad122b8931e42db1fb83b
Yaşar Mehmet Akat <akat434@gmail.com>
2023-07-29 11:51:52 +0300

    Result, DataResult ve Messages kısımları projeye eklendi.

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -10,8 +11,10 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll();
-        Product GetById(int productId);
-        List<ProductDetailDto> GetProductDetails();
+        IDataResult<List<Product>> GetAll();
+        IDataResult<Product> GetById(int productId);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
+        IResult Add(Product product);
+       IDataResult<List<ProductDetailDto>> GetProductDetails();
     }
 }
---
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class ErrorDataResult<T>:DataResult<T>
+    {
+        public ErrorDataResult(T data, string messages) : base(data, false, messages)
+        {
+
+        }
+
+        public ErrorDataResult(T data) : base(data, false)
+        {
+
+        }
+
+        public ErrorDataResult(string messages) : base(default, false, messages)
+        {
+
+        }
+
+        public ErrorDataResult() : base(default, false)
+        {
+
+        }
+    }
+}
